1b25d609641b44a972db2f51080d6d7cb95af4da
michaelssingh <michael@example.com>
2026-05-15 16:20:56 -0400

    fix: correctly write nix.custom.conf in entrypoint

--- builder/entrypoint.sh
+++ builder/entrypoint.sh
@@ -37,6 +37,15 @@ fi
 
 # 4. Start Nix Daemon in background
 echo "❄️ Starting Nix Daemon..."
+
+# Configure custom nix settings
+mkdir -p /etc/nix
+cat <<EOF > /etc/nix/nix.custom.conf
+builders-use-substitutes = true
+trusted-substituters = https://cache.nixos.org https://cache.flakehub.com
+extra-trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM= cache.flakehub.com-4:Asi8qIv291s0aYLyH6IOnr5Kf6+OF14WVjkE6t3xMio= cache.flakehub.com-5:zB96CRlL7tiPtzA9/WKyPkp3A2vqxqgdgyTVNGShPDU= cache.flakehub.com-6:W4EGFwAGgBj3he7c5fNh9NkOXw0PUVaxygCVKeuvaqU= cache.flakehub.com-7:mvxJ2DZVHn/kRxlIaxYNMuDG1OvMckZu32um1TadOR8= cache.flakehub.com-8:moO+OVS0mnTjBTcOUh2kYLQEd59ExzyoW1QgQ8XAARQ= cache.flakehub.com-9:wChaSeTI6TeCuV/Sg2513ZIM9i0qJaYsF+lZCXg0J6o= cache.flakehub.com-10:2GqeNlIp6AKp4EF2MVbE1kBOp9iBSyo0UPR9KoR0o1Y=
+EOF
+
 /nix/var/nix/profiles/default/bin/nix-daemon &
 
 # 5. Start SSHD in foreground
