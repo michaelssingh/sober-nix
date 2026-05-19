72d20b611eb94612309be704ddc6e88e1ba735ab
michaelssingh <michael@example.com>
2026-05-18 23:28:37 -0400

    feat(services): rename remote builder to sober-services and deploy soju

--- modules/home/core/irc/default.nix
+++ modules/home/core/irc/default.nix
@@ -5,12 +5,9 @@
     # Senpai configuration for Libera.Chat via Soju Bouncer
     # Managed declaratively by Home Manager
     
-    address ircs://irc.libera.chat:6697
+    address irc+insecure://sober-services.internal:6697
     nickname init_
     username init
     password "dT4d8y3Tz*kavNrmue4YzDsX3^VdU%9UA%8U"
-
-    # Auto-join channels (Note: Remove this once migrated to Soju bouncer)
-    channel "#nixos #linux #c ##anime ##rust #clojure #go #lobsters ##chat ##programming"
   '';
 }
