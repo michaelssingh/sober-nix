432f47df9077729299b1ca57f09f2c6f9cbe8afb
michaelssingh <michael@example.com>
2026-05-18 22:57:54 -0400

    fix(irc): handle nickname collision and set sasl username

--- modules/home/core/irc/default.nix
+++ modules/home/core/irc/default.nix
@@ -6,7 +6,8 @@
     # Managed declaratively by Home Manager
     
     address ircs://irc.libera.chat:6697
-    nickname init
+    nickname init_
+    username init
     password "dT4d8y3Tz*kavNrmue4YzDsX3^VdU%9UA%8U"
   '';
 }
