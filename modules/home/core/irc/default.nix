e4388eb287a668c5e3f50d45a48ac155ee6cfed2
michaelssingh <michael@example.com>
2026-05-18 22:45:31 -0400

    fix(irc): correct libera.chat server address

--- modules/home/core/irc/default.nix
+++ modules/home/core/irc/default.nix
@@ -5,7 +5,7 @@
     # Senpai configuration for Libera.Chat via Soju Bouncer
     # Managed declaratively by Home Manager
     
-    address ircs://libera.chat:6697
+    address ircs://irc.libera.chat:6697
     nickname init
     password "dT4d8y3Tz*kavNrmue4YzDsX3^VdU%9UA%8U"
   '';
