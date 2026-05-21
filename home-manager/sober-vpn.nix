cf435632f8cff15e892877171f5557d8303a0e2e
michaelssingh <michael@example.com>
2026-05-21 16:29:53 -0400

    fix: move vpn configuration to entrypoint to avoid home-manager nixos limitations

--- home-manager/sober-vpn.nix
+++ home-manager/sober-vpn.nix
@@ -1,28 +1,5 @@
 { pkgs, ... }:
-
 {
-  # Using systemd-managed network-online and networking for the interface
-  # We use systemd.network for a declarative approach
-  systemd.network.netdevs."10-wg-sober" = {
-    netdevConfig = {
-      Name = "wg-sober";
-      Kind = "wireguard";
-    };
-    wireguardConfig = {
-      PrivateKey = "0DGVDVBobtwR3yoX290O0naF1Wswmi2pbwJmnSV9L2w=";
-      ListenPort = 51820;
-    };
-    wireguardPeers = [
-      {
-        PublicKey = "mLpRrytjze69fuDpFkxwmYmB5ZBHyJKizfos9jyKWAM=";
-        AllowedIPs = [ "10.13.13.2/32" ];
-      }
-    ];
-  };
-
-  systemd.network.networks."10-wg-sober" = {
-    matchConfig.Name = "wg-sober";
-    address = [ "10.13.13.1/24" ];
-    networkConfig.IPForward = "yes";
-  };
+  # Placeholder module to maintain file existence 
+  # All VPN logic has been moved to builder/entrypoint.sh
 }
