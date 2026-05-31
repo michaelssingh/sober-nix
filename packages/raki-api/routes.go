98701ad0e9954e392bcd59a355fa1d1b71c2210e
michaelssingh <michael@example.com>
2026-05-31 10:06:42 -0400

    chore: remove duplicate routes.go in raki-api

--- packages/raki-api/routes.go
+++
@@ -1,9 +0,0 @@
-package adminapi
-
-import "net/http"
-
-func (s *Server) setupRoutes() {
-    s.mux.HandleFunc("/health", s.healthCheck)
-    s.mux.Handle("/api/users", s.authMiddleware(http.HandlerFunc(s.listUsers)))
-    // ... etc
-}
