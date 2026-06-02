3855583fe2e587d4a8126061cf073a597088200a
michaelssingh <michael@example.com>
2026-06-02 08:30:38 -0400

    chore: update to NixOS 26.05

--- modules/home/desktop/qutebrowser/default.nix
+++ modules/home/desktop/qutebrowser/default.nix
@@ -67,4 +67,15 @@ in
       };
     };
   };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "qutebrowser.desktop";
+      "x-scheme-handler/http" = "qutebrowser.desktop";
+      "x-scheme-handler/https" = "qutebrowser.desktop";
+      "x-scheme-handler/about" = "qutebrowser.desktop";
+      "x-scheme-handler/unknown" = "qutebrowser.desktop";
+    };
+  };
 }
