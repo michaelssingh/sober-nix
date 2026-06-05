49c4fb25c36ee4c6e37609cb1ace77e07c02c30f
Michael S. Singh <michael@sober.fyi>
2026-06-05 17:58:30 -0400

    refactor: apply production fixes to mkContainerImage builder and ignore core dumps

--- lib/default.nix
+++ lib/default.nix
@@ -10,6 +10,7 @@
     exposedPorts ? {},
     env ? {},
     users ? {},
+    groups ? {},
     harden ? true,
     usrBinEnv ? false,
   }:
@@ -17,52 +18,68 @@
     # Base shell - use minimal non-interactive bash for hardened builds
     shellPkg = if harden then pkgs.bash else pkgs.bashInteractive;
 
+    # Dynamically resolve the binary name of the shell
+    shellBinName = if pkgs.lib.hasPrefix "bash" (shellPkg.pname or "") then "bash" else (shellPkg.pname or "sh");
+    shellPath = "${shellPkg}/bin/${shellBinName}";
+
     # Core system tools always needed by the container runtime
     baseContents = [
       shellPkg
       pkgs.cacert
     ] ++ (if harden then [] else [ pkgs.coreutils ]);
 
-    # Write passwd and group files dynamically
+    # Write files avoiding formatting line-break issues
     passwdContent = ''
-      root:x:0:0::/root:${shellPkg}/bin/bash
-    '' + (pkgs.lib.concatStringsSep "\n" (pkgs.lib.mapAttrsToList (username: u:
-      "${username}:x:${toString u.uid}:${toString u.gid}:${u.description or username}:${u.home or "/var/empty"}:${u.shell or "${shellPkg}/bin/bash"}"
-    ) users)) + "\n";
+      root:x:0:0::/root:${shellPath}
+      ${pkgs.lib.concatStrings (pkgs.lib.mapAttrsToList (uName: u:
+        "${uName}:x:${toString u.uid}:${toString u.gid}:${u.description or uName}:${u.home or "/var/empty"}:${u.shell or shellPath}\n"
+      ) users)}'';
+
+    userGroups = pkgs.lib.mapAttrs' (username: u:
+      pkgs.lib.nameValuePair username { gid = u.gid; }
+    ) users;
+
+    allGroups = userGroups // groups;
 
     groupContent = ''
       root:x:0:
-    '' + (pkgs.lib.concatStringsSep "\n" (pkgs.lib.mapAttrsToList (groupname: g:
-      "${groupname}:x:${toString g.gid}:"
-    ) users)) + "\n";
+      ${pkgs.lib.concatStrings (pkgs.lib.mapAttrsToList (gName: g:
+        "${gName}:x:${toString g.gid}:\n"
+      ) allGroups)}'';
 
     passwdDir = pkgs.writeTextDir "etc/passwd" passwdContent;
     groupDir = pkgs.writeTextDir "etc/group" groupContent;
 
-    # Environment layout setup
+    # Environment layout components
     envDerivations = [
       passwdDir
       groupDir
     ] ++ (pkgs.lib.optional usrBinEnv (pkgs.runCommand "usr-bin-env" {} ''
       mkdir -p $out/usr/bin
-      ln -s ${pkgs.coreutils}/bin/env $out/usr/bin/env
+      ln -s ${if harden then "${pkgs.busybox}/bin/env" else "${pkgs.coreutils}/bin/env"} $out/usr/bin/env
     '')) ++ [
-      # Standard /tmp directory with sticky-bit permissions (required by standard application runtimes)
       (pkgs.runCommand "tmp-dir" {} ''
         mkdir -p $out/tmp
         chmod 1777 $out/tmp
       '')
     ];
 
+    # Consolidate configuration files into one single layer to prevent layer limit exceptions
+    envLayer = pkgs.symlinkJoin {
+      name = "${name}-env-layer";
+      paths = envDerivations;
+    };
+
     # Setup wrapper script
     entrypointScript = pkgs.writeShellScriptBin "entrypoint" ''
       set -e
-      # Execute the custom application startup commands
       ${entrypoint}
     '';
 
     standardEnv = {
-      PATH = "/bin";
+      PATH = pkgs.lib.makeBinPath (
+        baseContents ++ packages ++ [ entrypointScript ] ++ (if usrBinEnv && harden then [ pkgs.busybox ] else [])
+      );
       SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
     };
 
@@ -71,7 +88,7 @@
   pkgs.dockerTools.buildLayeredImage {
     inherit name tag;
 
-    contents = baseContents ++ envDerivations ++ [ entrypointScript ] ++ packages ++ extraContents;
+    contents = baseContents ++ [ envLayer entrypointScript ] ++ packages ++ extraContents;
 
     config = {
       Entrypoint = [ "${entrypointScript}/bin/entrypoint" ];
