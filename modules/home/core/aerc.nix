61da57b26082a5a676ad197ae4a0fcefc2a09005
Michael S. Singh <michael@sober.fyi>
2026-06-06 06:34:02 -0400

    style: enhance aerc UI layout, compose defaults, and w3m html filter

--- modules/home/core/aerc.nix
+++ modules/home/core/aerc.nix
@@ -7,8 +7,18 @@
         default-save-path = "~/Downloads";
         unsafe-accounts-conf = true;
       };
+      ui = {
+        sidebar-width = "20";
+        sidebar-visible = "true";
+        threading-enabled = "true";
+        index-format = "%D | %-15.15n | %s";
+      };
+      compose = {
+        editor = "nvim";
+        header-layout = "To,Cc,Subject";
+      };
       filters = {
-        "text/html" = "chawan";
+        "text/html" = "${pkgs.w3m}/bin/w3m -T text/html -dump";
       };
     };
   };
