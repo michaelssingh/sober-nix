6924eeb0914e77810fca42e85ba8d43a955d10bb
Michael S. Singh <michael@sober.fyi>
2026-06-07 16:55:35 -0400

    feat: codify bash config with host-sync activation script

--- users/init/hashnix.nix
+++ users/init/hashnix.nix
@@ -28,8 +28,48 @@
     nix # Ensure nix is in the profile path
   ];
 
-  # Shell Configuration (Manual management for host compatibility)
-  programs.bash.enable = false;
+  # Shell Configuration (Codified in HM, but synced as real files for host compatibility)
+  programs.bash = {
+    enable = true;
+    # These are our modern settings
+    shellAliases = {
+      hms = "home-manager switch --flake github:michaelssingh/sober-nix#init@hashnix --extra-experimental-features 'nix-command flakes' --refresh";
+      hack = "~/nix-user-chroot ~/.nix fish -l";
+      chat = "tmux -L host -f ~/.tmux-host.conf new-session -A -s host -n dream 'dream'";
+    };
+    bashrcExtra = ''
+      export CLICOLOR=1
+      export LSCOLORS=GxFxCxDxBxegedabagaced
+      alias ls='ls --color=auto'
+      alias ll='ls -lh'
+      alias la='ls -A'
+      alias grep='grep --color=auto'
+      HISTSIZE=5000
+      HISTFILESIZE=10000
+      HISTCONTROL=ignoreboth:erasedups
+      shopt -s histappend
+      PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+
+      if [ -f /etc/bash_completion ]; then
+        . /etc/bash_completion
+      fi
+    '';
+    profileExtra = ''
+      if [[ $- == *i* ]] && [ -z "$TMUX" ]; then
+        tmux -L host -f ~/.tmux-host.conf has-session -t host 2>/dev/null || tmux -L host -f ~/.tmux-host.conf new-session -d -s host -n dream 'dream'
+      fi
+    '';
+  };
+
+  # CRITICAL: This block copies the Bash config from the Nix store to real files.
+  # This prevents the "broken symlink" issue on the host shell while keeping
+  # the configuration codified in your Flake.
+  home.activation.syncBashConfig = config.lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    $DRY_RUN_CMD rm -f $HOME/.bashrc $HOME/.bash_profile
+    $DRY_RUN_CMD cp ${config.home.file.".bashrc".source} $HOME/.bashrc
+    $DRY_RUN_CMD cp ${config.home.file.".bash_profile".source} $HOME/.bash_profile
+    $DRY_RUN_CMD chmod 644 $HOME/.bashrc $HOME/.bash_profile
+  '';
 
   programs.fish = {
     enable = true;
