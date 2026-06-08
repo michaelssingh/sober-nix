118b2563eea68dafa0ccf4213b84055bb76746fb
Michael S. Singh <michael@sober.fyi>
2026-06-07 21:17:19 -0400

    feat(mosh): optimize experience with session variables and tmux function

---
+++ modules/home/core/mosh.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+
+{
+  programs.mosh = {
+    enable = true;
+  };
+
+  home.sessionVariables = {
+    # Disable predictive echo (the annoying underlines) globally
+    MOSH_PREDICTION_DISPLAY = "never";
+    # Allow Mosh to set the terminal title
+    MOSH_TITLE_TERM = "1";
+  };
+
+  # Optional: Alias for quick mosh + tmux attachment
+  # Usage: mt <host>
+  programs.fish.functions = {
+    mt = ''
+      mosh $argv -- tmux new-session -A -s main
+    '';
+  };
+}
