621103831cff176b29581cdba6913e2b2d09d042
Michael S. Singh <michael@sober.fyi>
2026-06-09 05:32:42 -0400

    feat: add theme styling to neomutt

--- modules/home/core/neomutt.nix
+++ modules/home/core/neomutt.nix
@@ -1,4 +1,11 @@
-_: {
+{
+  config,
+  ...
+}:
+let
+  colors = config.sober.theme.current.colors;
+in
+{
   programs.neomutt = {
     enable = true;
     vimKeys = true;
@@ -9,6 +16,18 @@ _: {
       bind index <down> sidebar-next
       bind index <right> sidebar-open
       bind index <left> sidebar-toggle-visible
+
+      # Theme
+      color index ${colors.fg} default ".*"
+      color index_author ${colors.blue} default ".*"
+      color index_number ${colors.comment} default
+      color index_subject ${colors.fg} default ".*"
+      color sidebar_indicator ${colors.accent} ${colors.bg_dark}
+      color sidebar_highlight ${colors.fg} ${colors.bg_visual}
+      color sidebar_divider ${colors.comment} ${colors.bg_dark}
+      color status ${colors.fg_sidebar} ${colors.bg_sidebar}
+      color indicator ${colors.accent} ${colors.bg_visual}
+      color tree ${colors.comment} default
 
       # HTML Rendering
       auto_view text/html
