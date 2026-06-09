8d47a9e650bde289f416aa7e7ccda11252301092
Michael S. Singh <michael@sober.fyi>
2026-06-09 00:01:29 -0400

    fix: remove redundant resolv.conf bind in aerc bwrap filter

--- modules/home/core/aerc.nix
+++ modules/home/core/aerc.nix
@@ -20,7 +20,7 @@
       filters = {
         "text/plain" = "wrap -w 100 | colorize";
         "text/html" =
-          "${pkgs.bubblewrap}/bin/bwrap --unshare-net --dev-bind / / --ro-bind-try /etc/resolv.conf /etc/resolv.conf ${pkgs.w3m}/bin/w3m -T text/html -O UTF-8 -o display_link_number=1 -dump | colorize";
+          "${pkgs.bubblewrap}/bin/bwrap --unshare-net --dev-bind / / ${pkgs.w3m}/bin/w3m -T text/html -O UTF-8 -o display_link_number=1 -dump | colorize";
       };
     };
   };
