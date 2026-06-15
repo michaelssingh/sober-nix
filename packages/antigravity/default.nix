9095d058180ecd9e8d5e196be4f78e1ad78eb889
Michael S. Singh <michael@sober.fyi>
2026-06-15 07:55:32 -0400

    config(agy): update antigravity CLI package to 1.0.8

--- packages/antigravity/default.nix
+++ packages/antigravity/default.nix
@@ -6,11 +6,11 @@
 
 stdenv.mkDerivation rec {
   pname = "antigravity";
-  version = "1.0.5";
+  version = "1.0.8";
 
   src = fetchurl {
-    url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.0.5-5009297080451072/linux-x64/cli_linux_x64.tar.gz";
-    sha512 = "72082d89ea71e101c7beb1630241428d53f68c702995897a2bbf55f162a9f71c8d2d7f98e7b310449cfbdb0b053f6e1b869c1f8d9fb23c95851e980d563d8924";
+    url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.0.8-5963827121094656/linux-x64/cli_linux_x64.tar.gz";
+    sha512 = "78426a61f9295d75285d9cdd39e9b9dc2736346468c92ce86a6fde1bcc7a9d6dd32c7bed1903a5deacd874a05d75babf5b907e991a117ad63f33548113c61bc2";
   };
 
   nativeBuildInputs = [ autoPatchelfHook ];
