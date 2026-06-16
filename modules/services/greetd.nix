82399d179332279f9f83c98f3bae6c3e6ecce154
Michael S. Singh <michael@sober.fyi>
2026-06-16 05:04:01 +0000

    fix(greetd): launch sway inside a login shell to load user environment

--- modules/services/greetd.nix
+++ modules/services/greetd.nix
@@ -4,7 +4,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd sway";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd 'bash -l -c sway'";
         user = "greeter";
       };
     };
