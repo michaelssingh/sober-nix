079a3f6f2d80a1714352c6acae83df752215bdc6
Michael S. Singh <michael@sober.fyi>
2026-06-25 23:32:38 +0000

    fix(clare): use --suffix for makeWrapper to allow user's mpv to override fallback, bump version to 0.1.10

--- packages/clare/default.nix
+++ packages/clare/default.nix
@@ -2,7 +2,7 @@
 
 buildGoModule {
   pname = "clare";
-  version = "0.1.9";
+  version = "0.1.10";
 
   src = ./.;
 
@@ -12,6 +12,6 @@ buildGoModule {
 
   postInstall = ''
     wrapProgram $out/bin/clare \
-      --prefix PATH : ${lib.makeBinPath [ ffmpeg yt-dlp mpv ]}
+      --suffix PATH : ${lib.makeBinPath [ ffmpeg yt-dlp mpv ]}
   '';
 }
