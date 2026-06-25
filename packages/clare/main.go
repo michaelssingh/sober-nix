9584f1a0b241334b26a5c68d94025e76be9863d8
Michael S. Singh <michael@sober.fyi>
2026-06-25 23:36:43 +0000

    chore(clare): bump main.go version to 0.1.10

--- packages/clare/main.go
+++ packages/clare/main.go
@@ -11,7 +11,7 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-const Version = "0.1.9"
+const Version = "0.1.10"
 
 func main() {
 	searchQuery := flag.String("s", "", "Search query for anime")
