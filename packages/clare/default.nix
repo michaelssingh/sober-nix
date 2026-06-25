1d83be95427f77677c6b559bb776073dcda940d3
Michael S. Singh <michael@sober.fyi>
2026-06-25 23:06:23 +0000

    bump(clare): version 0.1.4

--- packages/clare/default.nix
+++ packages/clare/default.nix
@@ -2,7 +2,7 @@
 
 buildGoModule {
   pname = "clare";
-  version = "0.1.3";
+  version = "0.1.4";
 
   src = ./.;
 
--- packages/clare/main.go
+++ packages/clare/main.go
@@ -11,7 +11,7 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-const Version = "0.1.3"
+const Version = "0.1.4"
 
 func main() {
 	searchQuery := flag.String("s", "", "Search query for anime")
