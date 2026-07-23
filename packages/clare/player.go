2f3ab4231695883b2fdfdd8909d9c329af958dd3
Michael S. Singh <michael@sober.fyi>
2026-07-23 22:01:38 +0000

    fix(clare): escape commas in protocol_whitelist option for MPV demuxer-lavf-o

--- packages/clare/player.go
+++ packages/clare/player.go
@@ -279,10 +279,10 @@ local skip_times_json = %q
 
 	if strings.Contains(streamURL, "m3u8") || strings.Contains(streamURL, "kotocdn") || strings.Contains(streamURL, "flikhub") {
 		args = append(args, "--no-ytdl")
-		args = append(args, "--demuxer-lavf-o-add=protocol_whitelist=file,http,https,tcp,tls,crypto,data,concat")
-		args = append(args, "--demuxer-lavf-o-add=allowed_segment_extensions=ALL")
-		args = append(args, "--demuxer-lavf-o-add=probesize=10000000")
-		args = append(args, "--demuxer-lavf-o-add=analyzeduration=10000000")
+		args = append(args, "--demuxer-lavf-o=protocol_whitelist=file\\,http\\,https\\,tcp\\,tls\\,crypto\\,data\\,concat")
+		args = append(args, "--demuxer-lavf-o=allowed_segment_extensions=ALL")
+		args = append(args, "--demuxer-lavf-o=probesize=10000000")
+		args = append(args, "--demuxer-lavf-o=analyzeduration=10000000")
 	} else {
 		args = append(args, "--ytdl-raw-options=user-agent="+UserAgent+",referer="+referer+",extractor-args=generic:impersonate")
 	}
