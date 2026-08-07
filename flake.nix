c75ca5271113a0c97a66919dc888d70a35869a44
Michael S. Singh <michael@sober.fyi>
2026-08-07 09:00:44 -0400

    refactor(flake): remove unused install-ninox package and app

--- flake.nix
+++ flake.nix
@@ -266,88 +266,6 @@
       };
 
       packages."x86_64-linux" = rec {
-        install-ninox = pkgs.writeShellScriptBin "install-ninox" ''
-          set -euo pipefail
-          echo "=== Starting Ninox NixOS Installation ==="
-          TARGET_DISK="''${1:-/dev/nvme0n1}"
-          echo "Target disk: $TARGET_DISK"
-
-          if [ ! -b "$TARGET_DISK" ]; then
-            echo "Error: Block device $TARGET_DISK does not exist!"
-            exit 1
-          fi
-
-          read -p "WARNING: All data on $TARGET_DISK will be wiped. Continue? (y/N) " -n 1 -r
-          echo
-          if [[ $REPLY =~ ^[Yy]$ ]]; then
-            echo "=== Partitioning $TARGET_DISK ==="
-            sgdisk -Z "$TARGET_DISK"
-            sgdisk -n 1:0:+1G -t 1:ef00 -c 1:ESP "$TARGET_DISK"
-            sgdisk -n 2:0:0 -t 2:8309 -c 2:luks "$TARGET_DISK"
-
-            PART1="''${TARGET_DISK}p1"
-            PART2="''${TARGET_DISK}p2"
-            if [ ! -b "$PART1" ]; then
-              PART1="''${TARGET_DISK}1"
-              PART2="''${TARGET_DISK}2"
-            fi
-
-            echo "=== Formatting EFI System Partition ==="
-            mkfs.vfat -F32 -n ESP "$PART1"
-
-            echo "=== Setting up LUKS Encryption ==="
-            cryptsetup luksFormat "$PART2"
-            cryptsetup open "$PART2" crypted
-
-            echo "=== Formatting Btrfs & Subvolumes ==="
-            mkfs.btrfs -f -L ninox /dev/mapper/crypted
-            mount /dev/mapper/crypted /mnt
-            btrfs subvolume create /mnt/@root
-            btrfs subvolume create /mnt/@home
-            btrfs subvolume create /mnt/@nix
-            umount /mnt
-
-            echo "=== Mounting Target Filesystems ==="
-            mount -o compress=zstd,noatime,subvol=@root /dev/mapper/crypted /mnt
-            mkdir -p /mnt/{boot,home,nix}
-            mount "$PART1" /mnt/boot
-            mount -o compress=zstd,noatime,subvol=@home /dev/mapper/crypted /mnt/home
-            mount -o compress=zstd,noatime,subvol=@nix /dev/mapper/crypted /mnt/nix
-
-            echo "=== Running NixOS Installation ==="
-            nixos-install --flake github:michaelssingh/sober-nix#ninox --no-channel-copy
-
-            echo "=== Provisioning SOPS Age Key ==="
-            mkdir -p /mnt/home/michael/.config/sops/age
-            if [ -f /home/nixos/.config/sops/age/keys.txt ]; then
-              cp /home/nixos/.config/sops/age/keys.txt /mnt/home/michael/.config/sops/age/keys.txt
-              chmod 600 /mnt/home/michael/.config/sops/age/keys.txt
-              echo "✓ Copied SOPS age key from live environment."
-            fi
-
-            echo "=== Provisioning sober-nix repository ==="
-            mkdir -p /mnt/home/michael/git
-            if [ ! -d /mnt/home/michael/git/sober-nix ]; then
-              echo "Cloning sober-nix repository into /home/michael/git/sober-nix..."
-              git clone https://github.com/michaelssingh/sober-nix.git /mnt/home/michael/git/sober-nix
-              chown -R 1000:100 /mnt/home/michael/git 2>/dev/null || true
-              echo "✓ Cloned sober-nix repository."
-            fi
-
-            echo "=== User Password Setup ==="
-            echo "Initial default password for user 'michael' is set to 'nixos'."
-            read -p "Would you like to set a custom password for user 'michael' now? (y/N) " -n 1 -r
-            echo
-            if [[ $REPLY =~ ^[Yy]$ ]]; then
-              nixos-enter --root /mnt -- passwd michael
-            fi
-
-            echo "=== Ninox installation complete! You may now reboot. ==="
-          else
-            echo "Installation cancelled."
-            exit 1
-          fi
-        '';
         ninox-iso = ninoxInstaller.config.system.build.isoImage;
         appservice-mgr = pkgs.callPackage ./tools/appservice-mgr { };
         tyto = pkgs.callPackage ./packages/tyto { };
@@ -378,14 +296,6 @@
           inherit pkgs soberLib publicKeys;
           inherit (nixpkgs) lib;
         };
-      };
-
-      apps."x86_64-linux" = rec {
-        install-ninox = {
-          type = "app";
-          program = "${ninoxInstaller.config.environment.systemPackages}/bin/install-ninox";
-        };
-        default = install-ninox;
       };
 
       homeConfigurations = {
