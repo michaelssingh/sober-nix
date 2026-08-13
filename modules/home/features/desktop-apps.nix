c57151e79ac3d0b58a21eeb8c5558c384343d8a2
Michael S. Singh <michael@sober.fyi>
2026-08-13 15:12:55 -0400

    style: remove zellij, terminus_font, and spleen

--- modules/home/features/desktop-apps.nix
+++ modules/home/features/desktop-apps.nix
@@ -13,44 +13,9 @@
     inputs.nixpkgs-pinned.legacyPackages.${pkgs.stdenv.hostPlatform.system}.transmission_4
     stig
     chawan
-    terminus_font
-    spleen
     qutebrowser
     antigravity
     strix-paste
     imv
   ];
-
-  # Terminal Multiplexer
-  programs.zellij = {
-    enable = true;
-    settings = {
-      theme = "tokyonight";
-      default_mode = "locked";
-      pane_frames = false;
-      mouse_mode = true;
-      copy_on_select = true;
-      mirror_session = true;
-
-      themes.tokyonight = {
-        fg = "#c0caf5";
-        bg = "#1a1b26";
-        black = "#15161e";
-        red = "#f7768e";
-        green = "#9ece6a";
-        yellow = "#e0af68";
-        blue = "#7aa2f7";
-        magenta = "#bb9af7";
-        cyan = "#7dcfff";
-        white = "#a9b1d6";
-        orange = "#ff9e64";
-      };
-
-      ui = {
-        pane_frames = {
-          rounded_corners = true;
-        };
-      };
-    };
-  };
 }
